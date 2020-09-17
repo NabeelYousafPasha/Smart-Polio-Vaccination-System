a25642d57ca37daebb493f62404eaaa919b5fc9e
Nabeel Yousaf Pasha <46818315+NabeelYousafPasha@users.noreply.github.com>
2020-09-18 00:19:43 +0500

    DEVELOP: added necessary models, controllers etc

---
+++ database/factories/ChildrenFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Children;
+use Faker\Generator as Faker;
+
+$factory->define(Children::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
