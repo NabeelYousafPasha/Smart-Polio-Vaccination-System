263fe10a5f1fa581d763a6c5cd9167318cd3e127
Nabeel Yousaf Pasha <46818315+NabeelYousafPasha@users.noreply.github.com>
2020-09-19 18:49:14 +0500

    DEVELOP: added routes and sidebar items

--- resources/views/dashboard/partials/sidebar.blade.php
+++ resources/views/dashboard/partials/sidebar.blade.php
@@ -10,7 +10,7 @@
                                 href="{{ route('/') }}"
                             >
                                 <strong class="font-bold">
-                                NABEEL
+                                {{ config('app.name', 'Laravel') }}
                                 </strong>
                             </a>
                         </span>
@@ -27,35 +27,14 @@
                 </div>
             </li>
 
-            @isset($sidebarNavs)
-                @foreach($sidebarNavs as $sidebarNav)
-                    <li class="{{ ( request()->is($sidebarNav['route_active'].'*') ) ? 'active' : '' }}">
-                        <a href="{{ $sidebarNav['children'] ? 'javascript:void(0)' : $sidebarNav['route'] }}">
-                            <i class="fa-fw {{ $sidebarNav['icon'] }}"></i>
-                            <span class="nav-label">
-                                {{ $sidebarNav['title'] }}
-                            </span>
-                            @if($sidebarNav['children'])
-                                <span class="fa-fw fa arrow"></span>
-                            @endif
-                        </a>
-                        @if($sidebarNav['children'])
-                            <ul class="nav nav-second-level collapse">
-                                @foreach($sidebarNav['children'] as $levelTwoNav)
-                                    <li class="{{ (request()->is($levelTwoNav['route_active'].'*') || request()->routeIs($levelTwoNav['route_name'])) ? 'active' : '' }}">
-                                        <a href="{{ $levelTwoNav['children'] ? 'javascript:void(0)' : $levelTwoNav['route'] }}">
-                                            <i class="fa-fw {{ $levelTwoNav['icon'] }}"></i>
-                                            <span class="nav-label custom-nav-label">
-                                                {{ $levelTwoNav['title'] }}
-                                            </span>
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        @endif
-                    </li>
-                @endforeach
-            @endisset
+            <li class="active">
+                <a href="javascript:void(0)">
+                    <i class="fa-fw fa fa-dashboard"></i>
+                    <span class="nav-label">
+                        {{ 'Dashboard' }}
+                    </span>
+                </a>
+            </li>
         </ul>
     </div>
 </nav>
