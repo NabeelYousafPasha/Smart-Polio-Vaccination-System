5069c160999c5798870afdb530eb5f68f7495286
Nabeel Yousaf Pasha <46818315+NabeelYousafPasha@users.noreply.github.com>
2020-09-20 16:41:00 +0500

    DEVELOP: added migrations

--- database/migrations/2020_09_15_163631_create_regions_table.php
+++ database/migrations/2020_09_15_163631_create_regions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWorkersTable extends Migration
+class CreateRegionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateWorkersTable extends Migration
      */
     public function up()
     {
-        Schema::create('workers', function (Blueprint $table) {
+        Schema::create('regions', function (Blueprint $table) {
             $table->id();
-            $table->string('cnic')->unique();
-            $table->foreignId('office_id')->constrained();
+            $table->string('name');
+            $table->foreignId('city_id')->constrained();
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreateWorkersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('workers');
+        Schema::dropIfExists('parentrefs');
     }
 }
